e42a60a5a2b17c472903f0a8f0b97b893fdd2e91
Sahar Al Seesi <alseesis1@southernct.edu>
2025-09-13 11:07:37 +0000

    adding the code we wrote in class

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const http = require('http');
+const url = require('url');
 
 const availableTimes = {
     Monday: ["1:00", "1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30"],
@@ -9,3 +11,44 @@ const availableTimes = {
 const appointments = [
     {name: "James", day: "Wednesday", time: "3:30" },
     {name: "Lillie", day: "Friday", time: "1:00" }];
+
+let serverObj =  http.createServer(function(req,res){
+	console.log(req.url);
+	let urlObj = url.parse(req.url,true);
+	switch (urlObj.pathname) {
+		case "/schedule":
+			schedule(urlObj.query,res);
+			break;
+		case "/cancel":
+			cancel(urlObj.query,res);
+			break;
+		default:
+			error(res,404,"pathname unknown");
+
+	}
+});
+
+function schedule(qObj,res) {
+	if (availableTimes[qObj.day].some(time => time == qObj.time))
+	{
+		res.writeHead(200,{'content-type':'text/plain'});
+		res.write("scheduled");
+		res.end();
+	}
+	else 
+		error(res,400,"Can't schedule");
+
+ 
+}
+
+function cancel(qObj) {
+}
+
+function error(response,status,message) {
+
+	response.writeHead(status,{'content-type':'text/plain'});
+	response.write(message);
+	response.end();
+}
+
+serverObj.listen(80,function(){console.log("listening on port 80")});
